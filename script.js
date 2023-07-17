be432073e12e0bc410c58c89cb3333db99f1e926
rishabhdiwaker <rishabhdiwaker.rd@gmail.com>
2023-07-18 00:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Person(name, age) {
 	this.age=age;
 }
 Person.prototype.greet=function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 }
 
 function Employee(name, age, jobTitle) {
@@ -16,7 +16,7 @@ Employee.prototype=Object.create(Person.prototype);
 Employee.prototype.constructor=Employee;
 
 Employee.prototype.jobGreet=function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 const person = new Person("Alice", 25);
